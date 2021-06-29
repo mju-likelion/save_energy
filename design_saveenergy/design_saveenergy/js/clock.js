c7b929bf7a4b05633246ce3b32eb539cc75363fb
seongyeon <ysy2565@naver.com>
2021-06-30 00:12:27 +0900

    modify clock

--- design_saveenergy/design_saveenergy/js/clock.js
+++ design_saveenergy/design_saveenergy/js/clock.js
@@ -2,20 +2,20 @@ window.onload = function () {
   var clock = document.getElementById('clock');
   setInterval(function () {
     var time = new Date().getHours() >= 12 ? "PM " : "AM "
-    time += new Date().getHours() > 12 ? new Date().getHours() - 12 : new Date().getHours();
+    time += new Date().getHours() >= 13 ? new Date().getHours() - 12 : new Date().getHours();
     time += " : " + new Date().getMinutes();
-    // time += new Date().getHours()- + ":" + new Date().getMinutes(); // 13~24시 표현
+    // time += new Date().getHours() + ":" + new Date().getMinutes(); // 13~24시 표현
     document.getElementById("clock").innerHTML = time;
   }, 1000);
   var month = new Date().getMonth();
   var day = new Date().getDay();
   var hour = new Date().getHours();
   var minute = new Date().getMinutes();
-  if (month == 04 && day == 22) {
-    document.body.style.backgroundImage = "url('/earth.jpg')";
-    // document.body.style.backgroundColor="black";
+  if (month == 6 && day == 30) {
+    // document.body.style.backgroundImage = "url('/earth.jpg')";
+    document.body.style.backgroundColor="black";
   }
-  if(hour==16 && minute==22 ){
+  if(hour == 16 && minute == 22 ){
     document.getElementById("event_text").innerHTML  = "현재 시간은 4시 22분입니다.<br> 4월 22일은 지구의 날입니다!"
   }
 };

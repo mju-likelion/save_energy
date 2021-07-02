a4f41dedc656a2052288f1e89dbd48aa505f9b35
seongyeon <ysy2565@naver.com>
2021-07-02 23:53:43 +0900

    modify clock

--- SAVE_ENERGY/js/clock.js
+++ SAVE_ENERGY/js/clock.js
@@ -1,21 +1,18 @@
-window.onload = function () {
-  setInterval(function () {
-    var time = new Date().getHours() >= 12 ? "PM " : "AM "
-    time += new Date().getHours() >= 13 ? new Date().getHours() - 12 : new Date().getHours();
-    time += " : " + new Date().getMinutes();
-    // time += new Date().getHours() + ":" + new Date().getMinutes(); // 13~24시 표현
-    document.getElementById("clock").innerHTML = time;
-  }, 1000);
-  var month = new Date().getMonth() + 1;
-  var day = new Date().getDate();
-  var hour = new Date().getHours();
-  var minute = new Date().getMinutes();
-  if (month == 7 && day == 2) {
-    // document.body.style.backgroundColor= 'black';
-    // document.body.style.backgroundImage = 'url("images/background_home_dark.jpg")';
-    document.getElementsByClassName("backimage").style.backgroundImage = 'url("images/background_home_dark.jpg")';
-  }
-  if (hour == 16 && minute == 22) {
-    document.getElementById("event_text").innerHTML = "현재 시간은 4시 22분입니다.<br> 4월 22일은 지구의 날입니다!"
-  }
-};
+setInterval(function () {
+  var time = new Date().getHours() >= 12 ? "PM " : "AM "
+  time += new Date().getHours() >= 13 ? new Date().getHours() - 12 : new Date().getHours();
+  time += " : " + new Date().getMinutes();
+  // time += new Date().getHours() + ":" + new Date().getMinutes(); // 13~24시 표현
+  document.getElementById("clock").innerHTML = time;
+}, 1000);
+var month = new Date().getMonth() + 1;
+var day = new Date().getDate();
+var hour = new Date().getHours();
+var minute = new Date().getMinutes();
+if (month == 7 && day == 2) {
+  var ele = document.getElementsByClassName("backimage")[0];
+  ele.style.backgroundImage = "url(images/background_home_dark.jpg)"
+}
+if (hour == 16 && minute == 22) {
+  document.getElementById("event_text").innerHTML = "현재 시간은 4시 22분입니다.<br> 4월 22일은 지구의 날입니다!"
+}

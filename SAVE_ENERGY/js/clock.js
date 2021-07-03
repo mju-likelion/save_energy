bad0818c9de190cad981e5629180a0e8e90872bc
seongyeon <ysy2565@naver.com>
2021-07-03 15:17:41 +0900

    login&logout modify

--- SAVE_ENERGY/js/clock.js
+++ SAVE_ENERGY/js/clock.js
@@ -9,9 +9,12 @@ var month = new Date().getMonth() + 1;
 var day = new Date().getDate();
 var hour = new Date().getHours();
 var minute = new Date().getMinutes();
-if (month == 7 && day == 2) {
+if (month == 4 && day == 22) {
   var ele = document.getElementsByClassName("backimage")[0];
   ele.style.backgroundImage = "url(images/background_home_dark.jpg)"
+}
+else{
+  
 }
 if (hour == 16 && minute == 22) {
   document.getElementById("event_text").innerHTML = "현재 시간은 4시 22분입니다.<br> 4월 22일은 지구의 날입니다!"

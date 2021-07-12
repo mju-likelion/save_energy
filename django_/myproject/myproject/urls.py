8056624b599455e1cc9c7c2fa0bf40be3dbafb88
seongyeon <ysy2565@naver.com>
2021-07-12 22:30:06 +0900

    modify django

---
+++ django_/myproject/myproject/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+import myapp.views
+import account.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', myapp.views.home, name="home"),
+    path('community/', myapp.views.community, name="community"),
+    path('introduce/', myapp.views.introduce, name="introduce"),
+    path('support/', myapp.views.support, name="support"),
+
+    path('login/', account.views.login, name='login'),
+    path('signup/', account.views.signup, name='signup'),
+    path('logout/', account.views.logout, name='logout'),
+]

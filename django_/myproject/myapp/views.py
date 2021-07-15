ec8df258628be6d619a5b8296959234485379881
seoyeon <silversun0808@gmail.com>
2021-07-15 20:26:07 +0900

    add detail fun & modify community_detail

--- django_/myproject/myapp/views.py
+++ django_/myproject/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Community
 # Create your views here.
 
@@ -12,6 +12,10 @@ def introduce(request):
 def community(request):
   communitys = Community.objects   # query set
   return render(request, 'community.html', {'communitys' : communitys})
+
+def detail(request, community_id) :
+  details = get_object_or_404(Community, pk=community_id)
+  return render(request, 'community_detail.html', {'details' : details})
 
 def support(request):
   return render(request, 'support.html')
